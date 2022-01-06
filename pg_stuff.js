0a18aee1ae2f06d8397e1435ada0718668dc0d83
Sean Rhee <seanrhee2024@u.northwestern.edu>
2022-01-06 16:18:15 -0500

    pleaseseeee

--- pg_stuff.js
+++ pg_stuff.js
@@ -1,9 +1,9 @@
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'blood-essential-loss-postgresql-headless.sean.svc.cluster.local',
-  database: 'tododb',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: '5432',
 });
