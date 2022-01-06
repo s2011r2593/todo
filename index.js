892930e25fbce69a4fc01bce7692ba873c1c57d7
Sean Rhee <seanrhee2024@u.northwestern.edu>
2022-01-06 14:18:23 -0500

    bruh

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 const tdldb = require('./pg_stuff');
 
-app.use(express.json())
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'blood-essential-loss-postgresql-headless.sean.svc.cluster.local');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();
-})
+app.use(express.json());
+app.use(cors({
+  origin: 'blood-essential-loss-postgresql-headless.sean.svc.cluster.local',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.get('/', (req, res) => {
-  console.log('hi');
-  tdldb.makeTable();
+  tdldb.getItems();
 });
 
 app.listen(port, () => {
